fd0ec20351fdead49273bc490a28f5aead458943
Tung Pham <joseph.pham280996@gmail.com>
2024-04-25 21:08:21 -0400

    Add implementation of CollabFilterOneVectorPerItem

--- src_starter/CollabFilterOneVectorPerItem.py
+++ src_starter/CollabFilterOneVectorPerItem.py
@@ -1,10 +1,10 @@
-'''
+"""
 CollabFilterOneVectorPerItem.py
 
 Defines class: `CollabFilterOneVectorPerItem`
 
 Scroll down to __main__ to see a usage example.
-'''
+"""
 
 # Make sure you use the autograd version of numpy (which we named 'ag_np')
 # to do all the loss calculations, since automatic gradients are needed
@@ -20,8 +20,9 @@ from train_valid_test_loader import load_train_valid_test_datasets
 
 # No other imports specific to ML (e.g. scikit) needed!
 
+
 class CollabFilterOneVectorPerItem(AbstractBaseCollabFilterSGD):
-    ''' One-vector-per-user, one-vector-per-item recommendation model.
+    """One-vector-per-user, one-vector-per-item recommendation model.
 
     Assumes each user, each item has learned vector of size `n_factors`.
 
@@ -37,10 +38,10 @@ class CollabFilterOneVectorPerItem(AbstractBaseCollabFilterSGD):
     -----
     Inherits *__init__** constructor from AbstractBaseCollabFilterSGD.
     Inherits *fit* method from AbstractBaseCollabFilterSGD.
-    '''
+    """
 
     def init_parameter_dict(self, n_users, n_items, train_tuple):
-        ''' Initialize parameter dictionary attribute for this instance.
+        """Initialize parameter dictionary attribute for this instance.
 
         Post Condition
         --------------
@@ -48,23 +49,30 @@ class CollabFilterOneVectorPerItem(AbstractBaseCollabFilterSGD):
         * param_dict : dict
             Keys are string names of parameters
             Values are *numpy arrays* of parameter values
-        '''
-        random_state = self.random_state # inherited RandomState object
+        """
+        random_state = self.random_state  # inherited RandomState object
 
         # TODO fix the lines below to have right dimensionality & values
         # TIP: use self.n_factors to access number of hidden dimensions
         self.param_dict = dict(
             mu=ag_np.ones(1),
-            b_per_user=ag_np.ones(1), # FIX dimensionality
-            c_per_item=ag_np.ones(1), # FIX dimensionality
-            U=0.001 * random_state.randn(1), # FIX dimensionality
-            V=0.001 * random_state.randn(1), # FIX dimensionality
-            )
+            b_per_user=ag_np.ones(n_users),  # FIX dimensionality
+            c_per_item=ag_np.ones(n_items),  # FIX dimensionality
+            U=0.001 * random_state.randn(n_users, self.n_factors),  # FIX dimensionality
+            V=0.001 * random_state.randn(n_items, self.n_factors),  # FIX dimensionality
+        )
 
-
-    def predict(self, user_id_N, item_id_N,
-                mu=None, b_per_user=None, c_per_item=None, U=None, V=None):
-        ''' Predict ratings at specific user_id, item_id pairs
+    def predict(
+        self,
+        user_id_N,
+        item_id_N,
+        mu=None,
+        b_per_user=None,
+        c_per_item=None,
+        U=None,
+        V=None,
+    ):
+        """Predict ratings at specific user_id, item_id pairs
 
         Args
         ----
@@ -79,15 +87,19 @@ class CollabFilterOneVectorPerItem(AbstractBaseCollabFilterSGD):
         yhat_N : 1D array, size n_examples
             Scalar predicted ratings, one per provided example.
             Entry n is for the n-th pair of user_id, item_id values provided.
-        '''
+        """
         # TODO: Update with actual prediction logic
         N = user_id_N.size
-        yhat_N = ag_np.ones(N)
+        yhat_N = (
+            mu
+            + b_per_user[user_id_N]
+            + c_per_item[item_id_N]
+            + ag_np.sum(U[user_id_N] * V[item_id_N], axis=1)
+        )
         return yhat_N
 
-
     def calc_loss_wrt_parameter_dict(self, param_dict, data_tuple):
-        ''' Compute loss at given parameters
+        """Compute loss at given parameters
 
         Args
         ----
@@ -98,25 +110,32 @@ class CollabFilterOneVectorPerItem(AbstractBaseCollabFilterSGD):
         Returns
         -------
         loss : float scalar
-        '''
+        """
         # TODO compute loss
         # TIP: use self.alpha to access regularization strength
-        y_N = data_tuple[2]
-        yhat_N = self.predict(data_tuple[0], data_tuple[1], **param_dict)
-        loss_total = 0.0
-        return loss_total    
+        user_id_N, item_id_N, y_N = data_tuple
+        yhat_N = self.predict(user_id_N, item_id_N, **param_dict)
+        loss_total = ag_np.sum(ag_np.square(y_N - yhat_N))
+
+        # Add L2 regularization terms
+        for param_name in ["U", "V", "b_per_user", "c_per_item"]:
+            param = param_dict[param_name]
+            loss_total += self.alpha * ag_np.sum(ag_np.square(param))
+
+        return loss_total
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Load the dataset
-    train_tuple, valid_tuple, test_tuple, n_users, n_items = \
+    train_tuple, valid_tuple, test_tuple, n_users, n_items = (
         load_train_valid_test_datasets()
+    )
     # Create the model and initialize its parameters
     # to have right scale as the dataset (right num users and items)
     model = CollabFilterOneVectorPerItem(
-        n_epochs=10, batch_size=10000, step_size=0.1,
-        n_factors=2, alpha=0.0)
+        n_epochs=10, batch_size=10000, step_size=0.1, n_factors=2, alpha=0.0
+    )
     model.init_parameter_dict(n_users, n_items, train_tuple)
 
     # Fit the model with SGD
